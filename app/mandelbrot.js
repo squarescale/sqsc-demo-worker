dfd9687189cfdce9656e08941d7adaa84b76deee
Romain Lenoel <romain.lenoel@gmail.com>
2017-08-22 13:39:15 +0200

    use only one canvas and use better understandable variable name

--- app/mandelbrot.js
+++ app/mandelbrot.js
@@ -16,28 +16,22 @@ function iterateMandelbrot(cx, cy, maxIter) {
   return i;
 }
 
-function fixColorByPoint(ppos, pix, i, iter) {
-  if (i == iter) {
-    pix[ppos] = 0;
-    pix[ppos + 1] = 0;
-    pix[ppos + 2] = 0;
-  } else {
-    var c = 3 * Math.log(i) / Math.log(iter - 1.0);
+function getColor(iter, maxIter) {
+  let color = {r: 0, g: 0, b: 0, a: 255};
+  if (iter !== maxIter) {
+    let c = 3 * Math.log(iter) / Math.log(maxIter - 1.0);
     if (c < 1) {
-      pix[ppos] = 255 * c;
-      pix[ppos + 1] = 0;
-      pix[ppos + 2] = 0;
+      color.r = 255 * c;
     } else if (c < 2) {
-      pix[ppos] = 255;
-      pix[ppos + 1] = 255 * (c - 1);
-      pix[ppos + 2] = 0;
+      color.r = 255;
+      color.g = 255 * (c - 1);
     } else {
-      pix[ppos] = 255;
-      pix[ppos + 1] = 255;
-      pix[ppos + 2] = 255 * (c - 2);
+      color.r = 255;
+      color.g = 255;
+      color.b = 255 * (c - 2);
     }
   }
-  pix[ppos + 3] = 255;
+  return color;
 }
 
 let Mandelbrot = {
@@ -48,31 +42,28 @@ let Mandelbrot = {
       minY = data.y - data.scaleY,
       maxY = data.y + data.scaleY;
   
-    let canvas = new Canvas(data.width, data.height),
-      canvasCropped = new Canvas(data.stepX, data.height);;
-    let ctx = canvas.getContext("2d"),
-      ctxCropped = canvasCropped.getContext("2d");
-    let img = ctx.getImageData(0, 0, data.width, data.height);
-    let pix = img.data;
-  
-    for (var ix = data.step; ix < data.step + data.stepX; ++ix) {
-      for (var iy = 0; iy < data.height; ++iy) {
-        let x = minX + (maxX - minX) * ix / (data.width - 1);
-        let y = minY + (maxY - minY) * iy / (data.height - 1);
-        let ppos = 4 * (data.width * iy + ix);
-        fixColorByPoint(
-          ppos,
-          pix,
-          iterateMandelbrot(x, y, data.iter),
-          data.iter);
+    let canvas = new Canvas(data.stepX, data.height);
+    let ctx = canvas.getContext("2d");
+    let img = ctx.getImageData(0, 0, data.stepX, data.stepY);
+    let pixelsData = img.data;
+
+    for (let ix = data.step; ix < data.step + data.stepX; ++ix) {
+      for (let iy = 0; iy < data.stepY; ++iy) {
+        let globalX = minX + (maxX - minX) * ix / (data.width - 1);
+        let globalY = minY + (maxY - minY) * iy / (data.height - 1);
+        let rawPixelPosition = 4 * (data.stepX * iy + ix - data.step);
+        let iterationCount = iterateMandelbrot(globalX, globalY, data.iter);
+        let pixelColor = getColor(iterationCount, data.iter);
+        pixelsData[rawPixelPosition] = pixelColor.r;
+        pixelsData[rawPixelPosition + 1] = pixelColor.g;
+        pixelsData[rawPixelPosition + 2] = pixelColor.b;
+        pixelsData[rawPixelPosition + 3] = pixelColor.a;
       }
     }
+
     ctx.putImageData(img, 0, 0);
-    let imgCropped = ctx.getImageData(data.step, 0, data.step + data.stepX, data.height);
-    ctxCropped.putImageData(imgCropped, 0, 0);
-    return canvasCropped.toDataURL();
+    return canvas.toDataURL();
   }
 }
-
 
 module.exports = Mandelbrot;
