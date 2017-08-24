0e291c5b7038617cc66e5d48a4d74e345022f098
Romain Lenoel <romain.lenoel@gmail.com>
2017-08-24 10:16:33 +0200

    worker does not handle the database table schema

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function handleQueueConnection() {
 }
 
 async function handleDatabaseConnection() {
-  await db.sequelize.sync({alter: true});
+  await db.sequelize.sync();
 }
 
 async function initQueues(channel) {
