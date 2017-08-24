602143f46f5be88e0ce66e9d27099ee9669adad4
Kevin Delfour <delfour.k@gmail.com>
2017-08-24 14:35:44 +0200

    Add TU on Mandelbrot and on Sequelize model loading

--- test/app/mandelbrot.js
+++ test/app/mandelbrot.js
@@ -15,14 +15,21 @@ const data = {
   startY: 0,
   stepX: 5,
   stepY: 5,
-  iter: 1
+  iter: 10
 }
 const blackCubeBase64Representation = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAABmJLR0QA/wD/AP+gvaeTAAAAFUlEQVQImWNkYGD4z4AGmNAFqCAIAGcRAQmm4PXDAAAAAElFTkSuQmCC";
+const fractalCubeBase64Representation ="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAABmJLR0QA/wD/AP+gvaeTAAAAMElEQVQImXXMsREAMAjDQM3EgiybBZhAKQgdKdz8+YSJHgSsNzSERo3GgUHz99yaFygUMb60N/G8AAAAAElFTkSuQmCC";
 
 describe('Mandelbrot', function() {
   describe('#get()', function() {
     it('should return a base64 representation of a 5 x 5 mandelbrot fractal', function() {
-      expect(mandelbrot.get(data)).to.be.equal(blackCubeBase64Representation);
+      expect(mandelbrot.get(data)).to.be.equal(fractalCubeBase64Representation);
+    });
+
+    it('should return a base64 representation of a 5 x 5 black cube if iter is 1', function() {
+      const incorrectData = data;
+      incorrectData.iter = 1;
+      expect(mandelbrot.get(incorrectData)).to.be.equal(blackCubeBase64Representation);
     });
 
     it('should return a base64 representation of a 5 x 5 black cube if incorrect datas', function() {
@@ -30,7 +37,19 @@ describe('Mandelbrot', function() {
       incorrectData.x = "a";
       expect(mandelbrot.get(incorrectData)).to.be.equal(blackCubeBase64Representation);
     });
-    
+
+    it('should return a base64 representation of a 5 x 5 black cube if x is null', function() {
+      const incorrectData = data;
+      incorrectData.x = null;
+      expect(mandelbrot.get(incorrectData)).to.be.equal(blackCubeBase64Representation);
+    });
+
+    it('should return a base64 representation of a 5 x 5 black cube if scaleX > X datas', function() {
+      const incorrectData = data;
+      incorrectData.scaleX = 1000;
+      expect(mandelbrot.get(incorrectData)).to.be.equal(blackCubeBase64Representation);
+    });
+
     it('should throw a TypeError exception if nulled datas', function() {
       expect(function(){
         mandelbrot.get(null)}
